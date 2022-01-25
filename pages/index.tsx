503db20daf8a5bdc7f7d3d4361255d8fed2d5b2c
Eyvaz Ahmadzada <eyvaz.ahmedzade.12@gmail.com>
2022-01-25 14:11:00 +0400

    changed home layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,27 @@
-import React, { ReactElement } from 'react';
+import MainLayout from 'app/components/layouts/Main.layout';
+import React from 'react';
 import { useDispatch } from 'react-redux';
 
 import SearchBoxForHome from '../app/components/modules/SearchBoxHome';
 import { changeModalAction } from '../app/store/slices/User.slice';
 import * as Home_STY from '../app/styles/styled-components/base/pages/home.style';
 
-interface Props {}
-
-function Index({}: Props): ReactElement {
+const Index = () => {
   const dispatch = useDispatch()
 
   return (
-    <div className="container-fluid container-main">
-      <div className="row justify-content-center">
-        <Home_STY.Home_STYLE className="col-lg-8">
-          <SearchBoxForHome />
-          <img src="/img/home-about.png" alt="home" className="w-100" />
-          <button
-            onClick={() => dispatch(changeModalAction('register'))}
-            className="registerButton animate hover-darken active-darken"
-          >
-            Sign Up
-          </button>
-        </Home_STY.Home_STYLE>
-      </div>
-    </div>
+    <MainLayout>
+      <Home_STY.Home_STYLE>
+        <SearchBoxForHome />
+        <img src="/img/home-about.png" alt="home" className="w-100" />
+        <button
+          onClick={() => dispatch(changeModalAction('register'))}
+          className="registerButton animate hover-darken active-darken"
+        >
+          Sign Up
+        </button>
+      </Home_STY.Home_STYLE>
+    </MainLayout>
   )
 }
 
