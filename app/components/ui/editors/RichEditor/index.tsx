309ce1039abaf6431ca1b493b4b296227d40cbac
Eyvaz Ahmadzada <eyvaz.ahmedzade.12@gmail.com>
2022-02-02 19:25:35 +0400

    changes

--- app/components/ui/editors/RichEditor/index.tsx
+++ app/components/ui/editors/RichEditor/index.tsx
@@ -1,17 +1,56 @@
-import { DraftHandleValue, Editor, EditorState, RichUtils } from 'draft-js';
+import { CompositeDecorator, ContentBlock, DraftHandleValue, Editor, EditorState, RichUtils } from 'draft-js';
 import linkifyIt from 'linkify-it';
-import React, { useEffect, useState } from 'react';
+import React, { ReactNode, useRef, useState } from 'react';
 
 import Toolbar from './Toolbar';
 
+const ProductItem = ({ children }: { children: ReactNode; offsetKey: string }) => {
+  return (
+    <div
+      // contentEditable={false}
+      suppressContentEditableWarning
+      style={{ width: '100%', height: 40, background: '#2e3a59', borderRadius: 5 }}
+    >
+      <span>{children}</span>
+    </div>
+  )
+}
+
 const RichEditor = () => {
-  const [editorState, setEditorState] = useState(() => EditorState.createEmpty())
-  const linkify = linkifyIt()
-  const [productLinks, setProductLinks] = useState<{ [index: string]: boolean }>({})
-  const [addTimer, setAddTimer] = useState<any>()
+  const editorRef = useRef<any>()
+  const [shouldReplace, setShouldReplace] = useState(false)
+
+  const findProductLink = (contentBlock: ContentBlock, callback: Function) => {
+    const blockText = contentBlock.getText()
+    const links = linkifyIt().match(blockText)
+
+    if (links) {
+      links.forEach((link: any) => {
+        if (link.url.includes(window.location.origin) && shouldReplace) {
+          const start = blockText.indexOf(link.url)
+          callback(start, start + link.url.length)
+          // shouldReplace = false
+        }
+      })
+    }
+  }
+
+  // Add custom decorator for embedding products
+  const compositeDecorator = new CompositeDecorator([
+    // {
+    //   strategy: findProductLink,
+    //   component: ProductItem,
+    // },
+  ])
+  const [editorState, setEditorState] = useState(() => EditorState.createEmpty(compositeDecorator))
 
   const handleKeyCommand = (command: string, editorState: EditorState): DraftHandleValue => {
     const newState = RichUtils.handleKeyCommand(editorState, command)
+
+    if (command === 'split-block') {
+      setShouldReplace(true)
+      // forceRender()
+    }
 
     if (newState) {
       setEditorState(newState)
@@ -20,54 +59,51 @@ const RichEditor = () => {
     return 'not-handled'
   }
 
-  useEffect(() => {
-    // console.log(productLinks)
-  }, [productLinks])
-
-  const addLinkPreview = (link: string) => {
-    clearTimeout(addTimer)
-    if (!productLinks[link]) {
-      const tempTimer = setTimeout(() => {
-        console.log('added' + link)
-        setProductLinks({ ...productLinks, [link]: true })
-      }, 500)
-
-      setAddTimer(tempTimer)
-    }
-  }
-
-  const removeLinkPreview = (link: string) => {
-    const productLinksUpdated = { ...productLinks }
-    delete productLinksUpdated[link]
-    console.log('removed' + link)
-    setProductLinks(productLinksUpdated)
-  }
-
   const handleEditorStateChange = (editorState: EditorState) => {
-    const blocks = editorState.getCurrentContent().getBlockMap()
-
-    Object.keys(productLinks).forEach((link) => {
-      // If none of the blocks include the link
-      if (blocks.every((block) => !block?.includes(link))) {
-        removeLinkPreview(link)
-      }
-    })
-
-    // Match links pasted into the content
-    blocks.forEach((block) => {
-      const links = linkify.match(block?.get('text'))
-
-      // Check links and add preview
-      if (links) {
-        links.forEach((link: any) => {
-          if (link.url.includes(window.location.origin)) {
-            addLinkPreview(link.url)
-          }
-        })
-      }
-    })
-
     setEditorState(editorState)
+  }
+
+  const forceRender = () => {
+    var contentState = editorState.getCurrentContent()
+
+    var newEditorStateInstance = EditorState.createWithContent(contentState, compositeDecorator)
+
+    var copyOfEditorState = EditorState.set(newEditorStateInstance, {
+      selection: editorState.getSelection(),
+      undoStack: editorState.getUndoStack(),
+      redoStack: editorState.getRedoStack(),
+      lastChangeType: editorState.getLastChangeType(),
+    })
+    setEditorState(copyOfEditorState)
+  }
+
+  const handleBlockRenderer = (contentBlock: ContentBlock) => {
+    const blockText = contentBlock.getText()
+    const links = linkifyIt().match(blockText)
+
+    if (links) {
+      links.forEach((link: any) => {
+        if (link.url.includes(window.location.origin)) {
+          return {
+            component: ProductItem,
+            editable: false,
+            props: {
+              foo: 'bar',
+            },
+          }
+          // setTimeout(() => {
+          //   setShouldReplace(false)
+          //   return {
+          //     component: ProductItem,
+          //     editable: false,
+          //     props: {
+          //       foo: 'bar',
+          //     },
+          //   }
+          // }, 500)
+        }
+      })
+    }
   }
 
   return (
@@ -75,9 +111,11 @@ const RichEditor = () => {
       <Toolbar editorState={editorState} setEditorState={setEditorState} />
       <div className="RichEditor-editor">
         <Editor
+          ref={editorRef}
           editorState={editorState}
           onChange={handleEditorStateChange}
           handleKeyCommand={handleKeyCommand}
+          blockRendererFn={handleBlockRenderer}
         />
       </div>
     </div>
